721c41c680320e1b5f08aeee2f6d1f39b9e9f131
awsl1414 <3030994569@qq.com>
2026-05-30 13:19:54 +0800

    feat(web): restructure routes to Legado mode (/my replaces /settings)

---
+++ apps/web/app/my/sources/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { SourceWorkspace } from "@/features/source-manager";
+
+export default function SourcesPage() {
+	return (
+		<div className="-mx-4 -mt-11 md:-mx-6 lg:-mx-8">
+			<SourceWorkspace />
+		</div>
+	);
+}
